d25e87279803ac77071ae54842164ad185c78365
shashankkatte <shashankanand.katte@gmail.com>
2021-03-24 13:53:45 -0400

    src : Check to avoid duplicate save of profile id in mongo

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,14 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          //    we already have a user with profile id
+        } else {
+          //  save the new user
+          new User({ googleId: profile.id }).save();
+        }
+      });
     }
   )
 );
