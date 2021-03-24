d01d2b8d67533d906345984086ee3156ad0995dc
shashankkatte <shashankanand.katte@gmail.com>
2021-03-23 22:41:43 -0400

    src : change greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 //  create a route handler
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hi: "Im on heroku" });
 });
 
 const PORT = process.env.PORT || 5000;
