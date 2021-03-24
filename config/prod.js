ab3a1cd26a24905963c9142d69d8625cc44597ea
shashankkatte <shashankanand.katte@gmail.com>
2021-03-24 17:03:05 -0400

    infra: update mongo env key

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
