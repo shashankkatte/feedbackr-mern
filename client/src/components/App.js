f2394b7e0b3955245ad0260aa9e5d1657cfdf549
shashankkatte <shashankanand.katte@gmail.com>
2021-04-02 11:59:11 -0400

    src : Wire up react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurverNew = () => <h2>SurverNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return (<div>hi there</div>);
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
