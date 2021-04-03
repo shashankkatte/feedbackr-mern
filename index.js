2ab3fa01fdec93584a60fad09d16967c977e1439
shashankkatte <shashankanand.katte@gmail.com>
2021-04-03 13:20:08 -0400

    src : Configuration for production routing

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //  Express should serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express to serve up index.html if route is not recognized
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
