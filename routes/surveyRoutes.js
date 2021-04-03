86c2e305699d3689b52895604a92470b0666d799
shashankkatte <shashankanand.katte@gmail.com>
2021-04-03 18:33:15 -0400

    src : creating surveys in survey route and subdoc collections

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+const survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-  app.post('/api.surveys', requireLogin,(req, res) => {
+  app.post('/api.surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients
+        .split(',')
+        .map((email) => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
   });
 };
