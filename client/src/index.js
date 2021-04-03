65a7b07ef15b9f90f56a0a34cb21df78bf4d7824
shashankkatte <shashankanand.katte@gmail.com>
2021-04-02 22:35:06 -0400

    config : Swreup Stripe for payments and setup env variables for dev and prod

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,8 @@ import reducers from './reducers';
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
--- config/prod.js
+++ config/prod.js
@@ -4,4 +4,6 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 };
