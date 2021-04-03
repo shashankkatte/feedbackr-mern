0ff1db267dfa61def1a2e7d177049e43c838aa7d
shashankkatte <shashankanand.katte@gmail.com>
2021-04-03 19:07:18 -0400

    infra: sendgrid keys

--- config/prod.js
+++ config/prod.js
@@ -6,4 +6,5 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY,
 };
