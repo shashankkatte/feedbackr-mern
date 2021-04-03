c9340203a79b95ac17b4be7eb593707d3a71f17f
shashankkatte <shashankanand.katte@gmail.com>
2021-04-03 11:12:03 -0400

    src : Displaying user credits in the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,11 +9,22 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return <li><a href="/auth/google">Login With Google</a></li>;
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
       default:
         return [
-          <li key="1"><Payments /></li>,
-          <li key="2"><a href="/api/logout">Logout</a></li>
+          <li key="1">
+            <Payments />
+          </li>,
+          <li Key="3" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="2">
+            <a href="/api/logout">Logout</a>
+          </li>,
         ];
     }
   }
@@ -26,11 +37,9 @@ class Header extends Component {
             to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
           >
-             FeedbackR
+            FeedbackR
           </Link>
-          <ul className="right">
-            {this.renderContent()}
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
