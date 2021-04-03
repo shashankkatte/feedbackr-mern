bc58c4bd421685e33e4acc115bc90908babc0d0f
shashankkatte <shashankanand.katte@gmail.com>
2021-04-03 14:21:14 -0400

    src : adding relationships to schenmas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
-require('./models/user');
+require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
