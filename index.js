10edc1b3173b8075e09339014db6a4988d97739b
shashankkatte <shashankanand.katte@gmail.com>
2021-04-03 14:28:34 -0400

    src : Adding Survey routes

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use(passport.session());
 //  create a route handler
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   //  Express should serve up production assets
