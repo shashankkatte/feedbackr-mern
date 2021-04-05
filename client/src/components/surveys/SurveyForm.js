7cd31e3dabe8394fc42cd122241734403c07101c
shashankkatte <shashankanand.katte@gmail.com>
2021-04-04 22:30:05 -0400

    src : adding form fields components

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+  renderFields() {
+    return (
+      <div>
+        <Field type="text" name="title" component={SurveyField} />
+      </div>
+    );
+  }
+
   render() {
-    return <div>SurveyForm!</div>;
+    return (
+      <div>
+        <form
+          onSubmit={this.props.handleSubmit((values) => console.log(values))}
+        >
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
