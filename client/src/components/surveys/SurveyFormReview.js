4fe6504b756b381b2cdff2832557e7431ba91850
shashankkatte <shashankanand.katte@gmail.com>
2021-04-04 23:39:22 -0400

    src : retreating forms

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,17 @@
 // SurveyFormReview shows users their form inputs for review
 import React from 'react';
 
-const SurveyReview = () => {
+const SurveyFormReview = ({ onCancel }) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      <button
+        className="yellow darken-3 white-text btn-flat"
+        onClick={onCancel}
+      >
+        Back
+      </button>
     </div>
   );
 };
-export default SurveyReview;
+export default SurveyFormReview;
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -8,9 +8,12 @@ class SurveyNew extends Component {
 
   renderContent() {
     if (this.state.showFormReview) {
-      return <SurveyFormReview />;
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false })}
+        />
+      );
     }
-
     return (
       <SurveyForm
         onSurveySubmit={() => this.setState({ showFormReview: true })}
