e0eb18890e162a7cccbd7a2f515a6f528657b345
shashankkatte <shashankanand.katte@gmail.com>
2021-04-04 23:46:26 -0400

    src : Persisting form values

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -60,4 +60,5 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'surveyForm',
+  destroyOnUnmount: false,
 })(SurveyForm);
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,5 +1,6 @@
 // SurveyFormReview shows users their form inputs for review
 import React from 'react';
+import { connect } from 'react-redux';
 
 const SurveyFormReview = ({ onCancel }) => {
   return (
@@ -14,4 +15,9 @@ const SurveyFormReview = ({ onCancel }) => {
     </div>
   );
 };
-export default SurveyFormReview;
+function mapStateToProps(state) {
+  console.log(state);
+  return { formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
