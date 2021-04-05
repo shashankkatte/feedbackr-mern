2a2397b70da608c27d907e1869e5ac8c2ab9694e
shashankkatte <shashankanand.katte@gmail.com>
2021-04-05 00:03:59 -0400

    src : wire up redux actions for form review

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,5 +9,10 @@ export const fetchUser = () => async (dispatch) => {
 
 export const handleToken = (token) => async (dispatch) => {
   const res = await axios.post('/api/stripe', token);
+
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = (values) => async (dispatch) => {
+  return { type: 'submit_survey' };
 };
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,9 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
@@ -24,6 +25,13 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
       >
         Back
       </button>
+      <button
+        onClick={() => submitSurvey(formValues)}
+        className="green btn-flat right white-text"
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
+      </button>
     </div>
   );
 };
@@ -32,4 +40,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
