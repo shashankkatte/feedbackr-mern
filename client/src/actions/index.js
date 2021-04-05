4e2c46595390f099cc95f1cfb9e7bc546c5fc9d4
shashankkatte <shashankanand.katte@gmail.com>
2021-04-05 00:16:40 -0400

    src: Posting to surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => async (dispatch) => {
+export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const handleToken = (token) => async (dispatch) => {
+export const handleToken = token => async dispatch => {
   const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvey = (values) => async (dispatch) => {
-  return { type: 'submit_survey' };
+export const submitSurvey = values => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
