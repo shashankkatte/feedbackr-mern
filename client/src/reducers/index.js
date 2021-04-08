a14d976b8fe9fa4b7ba07d791bc298f945380166
shashankkatte <shashankanand.katte@gmail.com>
2021-04-08 12:01:26 -0400

    src : Rendering list of surveys

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveyReducer from './surveysReducer';
 
 export default combineReducers({
   auth: authReducer,
   form: reduxForm,
+  surveys: surveyReducer,
 });
