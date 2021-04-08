25dd5a493fda5a638b29e681b51883468096cdbf
shashankkatte <shashankanand.katte@gmail.com>
2021-04-08 11:37:58 -0400

    src : Fetching surveys from DB

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -12,6 +12,13 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false,
+    });
+    res.send(surveys);
+  });
+
   app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
